75e26828699961732bb0eb485ba46d600a1f36d8
gcorrea4 <gabrielcorreasouza46@gmail.com>
2025-11-20 15:26:42 -0300

    feat: remoção do CSS inline

--- assets/js/contatos.js
+++ assets/js/contatos.js
@@ -4,23 +4,24 @@ document.addEventListener('DOMContentLoaded', function() {
     const btnLocalizacao = document.getElementById('btn-localizacao');
     const btnRedes = document.getElementById('btn-redes');
 
-   
     const contentContatos = document.getElementById('content-contatos');
     const contentLocalizacao = document.getElementById('content-localizacao');
     const contentRedes = document.getElementById('content-redes');
 
-   
     function mostrarSecao(secaoParaMostrar) {
-       
-        contentContatos.style.display = 'none';
-        contentLocalizacao.style.display = 'none';
-        contentRedes.style.display = 'none';
+        
+      
+        
+        contentContatos.classList.add('oculto');
+        contentLocalizacao.classList.add('oculto');
+        contentRedes.classList.add('oculto');
         
        
-        secaoParaMostrar.style.display = 'block';
+        if (secaoParaMostrar) {
+            secaoParaMostrar.classList.remove('oculto');
+        }
     }
 
-    
     btnContatos.addEventListener('click', function() {
         mostrarSecao(contentContatos);
     });
