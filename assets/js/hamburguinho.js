9634059a0411f061c1c93420f1c202f92f34d5fd
Gabriel <gabrielcorreasouza46@gmail.com>
2026-03-19 09:24:45 -0300

    feat: ajustes no estilo da seção destaque e melhorias no script do menu hamburguer

--- assets/js/hamburguinho.js
+++ assets/js/hamburguinho.js
@@ -4,19 +4,19 @@ document.addEventListener("DOMContentLoaded", () => {
     const menuLateral = document.getElementById("menu-lateral");
     const overlayMenu = document.getElementById("overlay-menu");
 
-    // Função para abrir o menu
+    
     btnHamburguer.addEventListener("click", () => {
         menuLateral.classList.add("ativo");
         overlayMenu.classList.add("ativo");
     });
 
-    // Função para fechar o menu clicando no X
+   
     btnFechar.addEventListener("click", () => {
         menuLateral.classList.remove("ativo");
         overlayMenu.classList.remove("ativo");
     });
 
-    // Função para fechar o menu clicando fora dele (no fundo escuro)
+    
     overlayMenu.addEventListener("click", () => {
         menuLateral.classList.remove("ativo");
         overlayMenu.classList.remove("ativo");
